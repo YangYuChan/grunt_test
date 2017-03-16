f6fe11dfdbf63fb39235bd86ddcf9633930cfa4f
yuchan_yang@126.com <yuchan_yang@126.com>
2017-03-16 17:30:50 +0800

    auto execute

--- Gruntfile.js
+++ Gruntfile.js
@@ -43,35 +43,6 @@ module.exports = function(grunt){
                 dest:'dist/js/<%= pkg.name %> - <%= pkg.version %>.min.js' //dest 是目的地输出
             }
         },
-        //代码检查js语法
-        // jshint:{
-        // //     // options:{
-        // //     //     jshintrc:'.jshint.text'
-        // //     // },
-        // //     // build:['src/js/*js']
-
-        //      all: [
-        //         'src/**/*.js'
-        //      ],
-        //     options: {
-        //         globals: {
-        //             $: false,
-        //             jQuery: false
-        //         },
-        //             browser: true,            // browser environment
-        //             devel: true                // 
-        //             }
-        
-        // },
-        //代码检查css语法
-        // csslint:{
-        //     options:{
-        //         csslintrc:'.csslint.text'
-        //     },
-        //     build:['src/scss/*.scss']
- 
-        // },
-        //编译scss文件为css
          sass: { 
             dist: {
                 files: {
@@ -83,11 +54,18 @@ module.exports = function(grunt){
         watch:{
             build:{
                 files:['src/js/*.js','src/scss/*.scss'],
-                tasks:['sass','jshint','csslint','concat','cssmin','uglify'],
+                tasks:['sass','concat','cssmin','uglify'],
                 options:{spawn:false}
             },
+            //监听文件变化自动执行
+            client:{
+                options:{
+                    livereload:true
+                },
+                files:['lib/*.html','src/js/*.js','src/scss/*.scss']
+            },
             scripts: { 
-                files: [ 'src/**/*.scss' ], 
+                files: [ 'src/scss/*.scss' ], 
                 tasks: ['sass'] 
             }
 
@@ -99,8 +77,6 @@ module.exports = function(grunt){
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    // grunt.loadNpmTasks('grunt-contrib-jshint');
-    // grunt.loadNpmTasks('grunt-contrib-csslint');
     grunt.loadNpmTasks('grunt-contrib-watch');
     //告诉grunt当我们在终端输入grunt时需要做些什么
     grunt.registerInitTask('default',['sass','concat','cssmin','uglify','watch']);//先进行语法检查，如果没有问题，再合并，再压缩
